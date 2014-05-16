6ddbdc90d38a623c52360201368dcfb608925cfe
Dalen W Brauner <DalenWBrauner@gmail.com>
2014-05-15 22:05:07 -0400

    Complete rework that does nothing
    But at least I'm using Sprites now :D

--- Hello World/Hello World.py
+++ Hello World/Hello World.py
@@ -4,21 +4,28 @@ from pyglet.window import key, mouse
 # CONSTANTS
 RESOLUTIONS = [(640,480),(800,600),(1024,640),(1024,800),(1080,720),(1280,720),(1680,1050)]
 
-# SYSTEM SETTINGS
+# SETUP
 current_resolution = 0
-offsets = {'resolution_buttons':[0,0]}
-offsets['resolution_buttons'][0] = 640/2 - 266   # x
-offsets['resolution_buttons'][1] = 480/2 - 112   # y
-
-# OTHER
 window = pyglet.window.Window()
-image = pyglet.resource.image("resolution_buttons.png")
-label = pyglet.text.Label('Select your resolution',
-                          font_name='Arial',
-                          font_size=25,
-                          x=window.width,
-                          y=window.height)
+image0 = pyglet.resource.image("resolution_buttons.png")
+image1 = pyglet.resource.image("resolution_button_individual.png")
+image2 = pyglet.resource.image("resolution_labels.png")
 sfx = pyglet.resource.media('chestopen.wav', streaming=False)
+
+# SPRITE STUFF
+batch_of_sprites = pyglet.graphics.Batch()
+layer = [ pyglet.graphics.OrderedGroup(x) for x in xrange(2)]
+buttons = [ pyglet.sprite.Sprite(image1,
+                                 x = 54 + (x*76),
+                                 y = 128,
+                                 batch = batch_of_sprites,
+                                 group = layer[0])
+            for x in xrange(len(RESOLUTIONS))]
+label = pyglet.sprite.Sprite(image2,
+                             x = buttons[0].x, y = 128,
+                             batch = batch_of_sprites,
+                             group = layer[1])
+
 
 
 # FUNCTIONS
@@ -30,31 +37,29 @@ def change_size(whichRes):
     newRes = RESOLUTIONS[whichRes]
     
     # Set which resolution we're using
-    CURRENT_RESOLUTION = whichRes
+    current_resolution = whichRes
 
-    # Change the offset (button positions)
-    offsets['resolution_buttons'][0] = newRes[0]/2 - 76*len(RESOLUTIONS)/2   # x
-    offsets['resolution_buttons'][1] = newRes[1]/2 - 112   # y
+    # Change the sprites' positions
+    for e in xrange(len(buttons)):
+        buttons[e].x = newRes[0]/2 - 76*len(buttons)/2 + e*76
+        buttons[e].y = newRes[1]/2 - 112   # y
+        
+    label.x, label.y = buttons[0].x, buttons[0].y
 
     # Change the resolution
     window.set_size(newRes[0],newRes[1])
 
-##    # Print our result
-##    print newRes[0],"x",newRes[1],';',offsets['resolution_buttons']
-
 @window.event
 def on_mouse_press(x, y, button, modifiers):
-    bttn_x = offsets['resolution_buttons'][0]
-    bttn_y = offsets['resolution_buttons'][1]
+    # First, check they're even in the right y range.
+    bttn_y = buttons[0].y 
     if  (bttn_y) <= y <= (bttn_y+224):
-        if   (bttn_x + (76*0)) < x <= (bttn_x + (76*1)):   change_size(0)
-        elif (bttn_x + (76*1)) < x <= (bttn_x + (76*2)):   change_size(1)
-        elif (bttn_x + (76*2)) < x <= (bttn_x + (76*3)):   change_size(2)
-        elif (bttn_x + (76*3)) < x <= (bttn_x + (76*4)):   change_size(3)
-        elif (bttn_x + (76*4)) < x <= (bttn_x + (76*5)):   change_size(4)
-        elif (bttn_x + (76*5)) < x <= (bttn_x + (76*6)):   change_size(5)
-        elif (bttn_x + (76*6)) < x <= (bttn_x + (76*7)):   change_size(6)
         
+        # If they've actually clicked a buttons, react appropriately.
+        for e in xrange(len(buttons)):
+            if buttons[e].x < x <= buttons[e].x + 76:
+                change_size(e)
+                break
 
 @window.event
 def on_key_press(symbol, modifiers):
@@ -62,19 +67,8 @@ def on_key_press(symbol, modifiers):
 
 @window.event
 def on_draw():
-    # Set background color
-    window.clear()
-
-    # Draw the images at their specified positions
-    for each_button in buttons:
-        each_button.blit(offsets['resolution_buttons'][0],
-                         offsets['resolution_buttons'][1])
-    image.blit(offsets['resolution_buttons'][0],
-               offsets['resolution_buttons'][1])
-    
-    
-    # Draw the label
-    #label.draw()                       # Draw the label onto the window
+    window.clear()  # Set background color
+    batch_of_sprites.draw()    # Draw ALL the sprites!
 
 #window.push_handlers(pyglet.window.event.WindowEventLogger())
 pyglet.app.run()
