e49a23712ae2eb234fb3ed4f5818e7ac94ce0d7f
Dalen W Brauner <DalenWBrauner@gmail.com>
2014-06-05 14:12:49 -0400

    Added an 'export' button to demo saving the resolution

--- Hello World/Hello World.py
+++ Hello World/Hello World.py
@@ -10,7 +10,10 @@ window = pyglet.window.Window()
 image0 = pyglet.resource.image("resolution_buttons.png")
 image1 = pyglet.resource.image("resolution_button_individual.png")
 image2 = pyglet.resource.image("resolution_labels.png")
-sfx = pyglet.resource.media('chestopen.wav', streaming=False)
+image3 = pyglet.resource.image("resolution_button_individual_selected_rotated.png")
+image4 = pyglet.resource.image("save_label.png")
+sfx = pyglet.resource.media('boing.wav', streaming=False)
+save_sfx = pyglet.resource.media('coin.wav', streaming=False)
 
 # SPRITE STUFF
 batch_of_sprites = pyglet.graphics.Batch()
@@ -25,11 +28,22 @@ label = pyglet.sprite.Sprite(image2,
                              x = buttons[0].x, y = 128,
                              batch = batch_of_sprites,
                              group = layer[1])
+new_button = pyglet.sprite.Sprite(image3,
+                                  x = buttons[-1].x - 224 + 76,
+                                  y = buttons[0].y - 76,
+                                  batch = batch_of_sprites,
+                                  group = layer[0])
+new_label = pyglet.sprite.Sprite(image4,
+                                 x = new_button.x,
+                                 y = new_button.y,
+                                 batch = batch_of_sprites,
+                                 group = layer[1])
 
 
 
 # FUNCTIONS
 def change_size(whichRes):
+    '''The user has decided to change their resolution!'''
     # Play the sound effect
     sfx.play()
 
@@ -37,17 +51,27 @@ def change_size(whichRes):
     newRes = RESOLUTIONS[whichRes]
     
     # Set which resolution we're using
+    global current_resolution
     current_resolution = whichRes
 
-    # Change the sprites' positions
+    # Move the buttons to the center of the screen
     for e in xrange(len(buttons)):
         buttons[e].x = newRes[0]/2 - 76*len(buttons)/2 + e*76
         buttons[e].y = newRes[1]/2 - 112   # y
-        
+
+    # Adjust all other sprites accordingly
     label.x, label.y = buttons[0].x, buttons[0].y
+    new_label.x = new_button.x = buttons[-1].x - 224 + 76
+    new_label.y = new_button.y = buttons[0].y - 76
 
     # Change the resolution
     window.set_size(newRes[0],newRes[1])
+
+def export_res():
+    '''The user has decided to save their selection!'''
+    with open('saved_resolution.txt','w') as f:
+        f.write(str(RESOLUTIONS[current_resolution])+'\n')
+    save_sfx.play()
 
 @window.event
 def on_mouse_press(x, y, button, modifiers):
@@ -61,9 +85,13 @@ def on_mouse_press(x, y, button, modifiers):
                 change_size(e)
                 break
 
-@window.event
-def on_key_press(symbol, modifiers):
-    pass
+    # If they've clicked the GO/EXPORT button, export a .txt stating the resolution
+    elif  (bttn_y-76) <= y < (bttn_y) and (buttons[-1].x - 224 + 76 <= x <= buttons[-1].x + 76):
+        export_res()
+
+##@window.event
+##def on_key_press(symbol, modifiers):
+##    pass
 
 @window.event
 def on_draw():
