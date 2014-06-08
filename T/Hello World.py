144d20a7ca9d2f6704361099c60f28044c6b215e
Dalen W Brauner <DalenWBrauner@gmail.com>
2014-06-08 18:38:28 -0400

    Buttons click properly, even with buttons of various sizes.

--- T/Hello World.py
+++ T/Hello World.py
@@ -6,7 +6,7 @@ from pyglet.resource import media as pygMedia
 from pyglet.sprite import Sprite as pygSprite
 
 # CONSTANTS
-IMAGE_FILES = ('B_green90.png','B_orange.png','L_save.png')
+IMAGE_FILES = ('B_green.png','B_orange.png','B_green90.png','B_orange90.png','L_save.png')
 SOUND_FILES = ('boing.wav','coin.wav')
 
 # LOAD SOUND EFFECTS
@@ -26,8 +26,8 @@ Layer = [ pyglet.graphics.OrderedGroup(x) for x in xrange(10)]
 # WINDOW SETUP
 WINDOW = pyglet.window.Window()
 # Notice: window size is dynamic with image sizes
-button_width = max(( IMG['B_green90'].width , IMG['B_orange'].width ))
-button_height = max(( IMG['B_green90'].height , IMG['B_orange'].height ))
+button_width = max(( IMG['B_green'].width , IMG['B_orange'].width , IMG['B_orange90'].width , IMG['B_green90'].width ))
+button_height = max(( IMG['B_green'].height , IMG['B_orange'].height , IMG['B_orange90'].height , IMG['B_green90'].height))
 win_width = button_width * 4
 win_height = button_height + IMG['L_save'].height
 
@@ -35,13 +35,13 @@ WINDOW.set_size(win_width, win_height)
 
 # WE'RE GONNA HAVE FUN WITH THIS THING
 # Notice: Sprite positions are dynamic with image sizes
-buttons4 = [ pygSprite( IMG['B_green90'],
+buttons4 = [ pygSprite( IMG['B_green'],
                         x = 0,
                         y = IMG['L_save'].height,
                         batch = Batch,
                         group = Layer[0]
                         ),
-             pygSprite( IMG['B_orange'],
+             pygSprite( IMG['B_orange90'],
                         x = button_width,
                         y = IMG['L_save'].height,
                         batch = Batch,
@@ -68,43 +68,26 @@ save_label = pygSprite( IMG['L_save'],
                         )
 
 
-# FUNCTIONS
-##def change_size(whichRes):
-##    '''The user has decided to change their resolution!'''
-##    # Play the sound effect
-##    sfx.play()
-##
-##    # Grab the new resolution data
-##    newRes = RESOLUTIONS[whichRes]
-##    
-##    # Set which resolution we're using
-##    global current_resolution
-##    current_resolution = whichRes
-##
-##    # Move the buttons to the center of the screen
-##    for e in xrange(len(buttons)):
-##        buttons[e].x = newRes[0]/2 - 76*len(buttons)/2 + e*76
-##        buttons[e].y = newRes[1]/2 - 112   # y
-##
-##    # Adjust all other sprites accordingly
-##    label.x, label.y = buttons[0].x, buttons[0].y
-##    new_label.x = new_button.x = buttons[-1].x - 224 + 76
-##    new_label.y = new_button.y = buttons[0].y - 76
-##
-##    # Change the resolution
-##    window.set_size(newRes[0],newRes[1])
-##
-##def export_res():
-##    '''The user has decided to save their selection!'''
-##    with open('saved_resolution.txt','w') as f:
-##        f.write(str(RESOLUTIONS[current_resolution])+'\n')
-##    save_sfx.play()
+
 
 @WINDOW.event
 def on_mouse_press(x, y, button, modifiers):
-##    # First, check they're even in the right y range.
-##    bttn_y = buttons[0].y 
-##    if  (bttn_y) <= y <= (bttn_y+224):
+    # Check the y range first.
+    if y < IMG['L_save'].height:
+        print "SAVE"
+    elif x < button_width:
+        print 1
+    elif x < button_width*2:
+        print 2
+    elif x < button_width*3:
+        print 3
+    elif x < button_width*4:
+        print 4
+    else:
+        print "Nope.avi"
+        
+        
+        
 ##        
 ##        # If they've actually clicked a buttons, react appropriately.
 ##        for e in xrange(len(buttons)):
@@ -126,5 +109,8 @@ def on_draw():
     WINDOW.clear()  # Set background color
     Batch.draw()    # Draw ALL the sprites!
 
-WINDOW.push_handlers(pyglet.window.event.WindowEventLogger())
+def export():
+    print "I'M EXPORTING THINGS I SWEAR"
+
+#WINDOW.push_handlers(pyglet.window.event.WindowEventLogger())
 pyglet.app.run()
