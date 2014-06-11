18379370fd610a048dc79e613d562c2094dc345b
Dalen W Brauner <DalenWBrauner@gmail.com>
2014-06-11 13:29:10 -0400

    Added crappy button impressions

--- T/Hello World.py
+++ T/Hello World.py
@@ -6,7 +6,11 @@ from pyglet.resource import media as pygMedia
 from pyglet.sprite import Sprite as pygSprite
 
 # CONSTANTS
-IMAGE_FILES = ('B_green.png','B_orange.png','B_green90.png','B_orange90.png','L_save.png')
+IMAGE_FILES = ('B01_norm.png','B01_press.png','B01_selec.png',
+               'B02_norm.png','B02_press.png','B02_selec.png',
+               'B03_norm.png','B03_press.png','B03_selec.png',
+               'B04_norm.png','B04_press.png','B04_selec.png',
+               'B05_norm.png')
 SOUND_FILES = ('start.wav','go.wav')
 
 # LOAD SOUND EFFECTS
@@ -26,42 +30,50 @@ Layer = [ pyglet.graphics.OrderedGroup(x) for x in xrange(10)]
 # WINDOW SETUP
 WINDOW = pyglet.window.Window()
 # Notice: window size is dynamic with image sizes
-button_width = max(( IMG['B_green'].width , IMG['B_orange'].width , IMG['B_orange90'].width , IMG['B_green90'].width ))
-button_height = max(( IMG['B_green'].height , IMG['B_orange'].height , IMG['B_orange90'].height , IMG['B_green90'].height))
+button_width = max(
+    IMG['B01_norm'].width,
+    IMG['B02_norm'].width,
+    IMG['B03_norm'].width,
+    IMG['B04_norm'].width,)
+button_height = max(
+    IMG['B01_norm'].height,
+    IMG['B02_norm'].height,
+    IMG['B03_norm'].height,
+    IMG['B04_norm'].height,)
 win_width = button_width * 4
-win_height = button_height + IMG['L_save'].height
+win_height = button_height + IMG['B05_norm'].height
 
 WINDOW.set_size(win_width, win_height)
 
 # WE'RE GONNA HAVE FUN WITH THIS THING
 # Notice: Sprite positions are dynamic with image sizes
-buttons4 = [ pygSprite( IMG['B_green'],
+buttons4 = [ pygSprite( IMG['B01_norm'],
                         x = 0,
-                        y = IMG['L_save'].height,
+                        y = IMG['B05_norm'].height,
                         batch = Batch,
                         group = Layer[0]
                         ),
-             pygSprite( IMG['B_orange90'],
+             pygSprite( IMG['B02_norm'],
                         x = button_width,
-                        y = IMG['L_save'].height,
+                        y = IMG['B05_norm'].height,
                         batch = Batch,
                         group = Layer[0]
                         ),
-             pygSprite( IMG['B_green90'],
+             pygSprite( IMG['B03_norm'],
                         x = button_width *2,
-                        y = IMG['L_save'].height,
+                        y = IMG['B05_norm'].height,
                         batch = Batch,
                         group = Layer[0]
                         ),
-             pygSprite( IMG['B_orange'],
+             pygSprite( IMG['B04_norm'],
                         x = button_width *3,
-                        y = IMG['L_save'].height,
+                        y = IMG['B05_norm'].height,
                         batch = Batch,
                         group = Layer[0]
                         ),
              ]
-save_label = pygSprite( IMG['L_save'],
-                        x = (win_width/2) - (IMG['L_save'].width/2),
+save_label = pygSprite( IMG['B05_norm'],
+                        x = (win_width/2) - (IMG['B05_norm'].width/2),
                         y = 0,
                         batch = Batch,
                         group = Layer[0]
@@ -74,25 +86,35 @@ SFX['start'].play()
 def on_mouse_press(x, y, button, modifiers):
     global USER_PREF
     # Check the y range first.
-    if y < IMG['L_save'].height:
+    if y < IMG['B05_norm'].height:
         with open('saved_resolution.txt','w') as f:
             f.write(str(USER_PREF)+'\n')
         SFX['go'].play()
     elif x < button_width:
+        buttons4[0].image = IMG['B01_press']
         USER_PREF = 1
     elif x < button_width*2:
+        buttons4[1].image = IMG['B02_press']
         USER_PREF = 2
     elif x < button_width*3:
+        buttons4[2].image = IMG['B03_press']
         USER_PREF = 3
     elif x < button_width*4:
+        buttons4[3].image = IMG['B04_press']
         USER_PREF = 4
     else:
         USER_PREF = 'Nope.avi'
+
+@WINDOW.event
+def on_mouse_release(x, y, button, modifiers):
+    # If any of the buttons are being pressed, release them
+    pass
+
 
 @WINDOW.event
 def on_draw():
     WINDOW.clear()  # Set background color
     Batch.draw()    # Draw ALL the sprites!
 
-#WINDOW.push_handlers(pyglet.window.event.WindowEventLogger())
+WINDOW.push_handlers(pyglet.window.event.WindowEventLogger())
 pyglet.app.run()
